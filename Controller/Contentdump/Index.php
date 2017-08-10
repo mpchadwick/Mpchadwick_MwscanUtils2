43cea2e84bb156ea3cf4910756d0c2bea26d851f
Max Chadwick <mpchadwick@gmail.com>
2017-08-10 19:17:03 -0400

    Add CmsPage and CmsBlock models

--- Controller/Contentdump/Index.php
+++ Controller/Contentdump/Index.php
@@ -5,12 +5,16 @@ namespace Mpchadwick\MwscanUtils\Controller\Contentdump;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\Action\Context;
 use Magento\Config\Model\ResourceModel\Config\Data\Collection as ConfigDataCollection;
+use Magento\Cms\Api\Data\PageInterface as CmsPage;
+use Magento\Cms\Api\Data\BlockInterface as CmsBlock;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
     const SEPARATOR = PHP_EOL . PHP_EOL . '----' . PHP_EOL . PHP_EOL;
 
     protected $configDataCollection;
+    protected $cmsPage;
+    protected $cmsBlock;
 
     protected $paths = [
         'design/head/includes',
@@ -19,10 +23,14 @@ class Index extends \Magento\Framework\App\Action\Action
 
     public function __construct(
         Context $context,
-        ConfigDataCollection $configDataCollection
+        ConfigDataCollection $configDataCollection,
+        CmsPage $cmsPage,
+        CmsBlock $cmsBlock
     ) {
         parent::__construct($context);
         $this->configDataCollection = $configDataCollection;
+        $this->cmsPage = $cmsPage;
+        $this->cmsBlock = $cmsBlock;
     }
 
     public function execute()
